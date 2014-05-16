103c3677be1a9dfdf82e684710d60b033685eeee
Hampus Liljekvist <hlilje@users.noreply.github.com>
2014-05-16 10:16:50 +0200

    Update script

--- symlink.sh
+++ symlink.sh
@@ -1,8 +1,25 @@
 #! /bin/sh
-# A script to create symlinks from home to all the files in the current directory.
+# A script to create symlinks from home to all the dotfiles.
 
-#TODO
+dotfiles="$HOME/Dropbox/dotfiles/.*"
 
-echo "Symlinks created"
+for file in $dotfiles; do
+    f=`basename $file`
+    # Ignore certain 'files'
+    if [ "$f" != "." ] && [ "$f" != ".." ] && [ "$f" != ".git" ]
+    then
+        if [ ! -e ~/$f ] ; then
+            ln -s ~/Dropbox/dotfiles/$f ~/$f
+        else
+            echo "File $f already exists"
+        fi
+    fi
+done
+
+if [ $? -ne 0 ]; then
+    echo "There were errors"
+else
+    echo "Symlinks created without errors"
+fi
 
 exit 0
